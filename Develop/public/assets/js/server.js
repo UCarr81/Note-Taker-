8eee8f792e488b12fd97ab5cf91d7b9edc7cd5dc
Uriel <CarrilloUriel81@gmail.com>
2023-10-08 15:10:35 -0700

    Added PORT and Express to server.js

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express() 
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT:' + PORT);
+});
