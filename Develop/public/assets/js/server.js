61a1aad93a5aab784bd5484d2778a1b0ad63d654
Uriel <CarrilloUriel81@gmail.com>
2023-10-11 15:43:37 -0700

    added Routes for html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -6,16 +6,11 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
 
-
-
-
-
-
-
-
-
-
+require('/Develop/public/index.html')(app);
 
 
 app.listen(PORT, function() {
